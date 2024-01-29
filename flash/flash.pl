fa3be305ace3c33c0bbbf06d0b3bd85d5e10bec3
yuuki <95370823+yuuki15@users.noreply.github.com>
2024-01-30 00:18:03 +0900

    Create flash.pl

---
+++ flash/flash.pl
@@ -0,0 +1,28 @@
+use Time::HiRes qw(sleep);
+
+$| = 1;
+
+my $correct_answer = 0;
+
+for (1 .. 10) {
+    my $number = 1 + int(rand(9));
+
+    print "\r$number\a";
+    sleep 0.3;
+
+    print "\r";
+    sleep 0.2;
+
+    $correct_answer += $number;
+}
+
+print "\rSum?\n";
+my $answer = <>;
+chomp $answer;
+
+if ($answer == $correct_answer) {
+    print "Correct\n";
+}
+else {
+    print "$correct_answer\n";
+}
